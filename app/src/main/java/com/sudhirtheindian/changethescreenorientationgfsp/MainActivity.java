205df17efce88094486007154686be37fb98edf8
sudhir <sudhirpraj0603@gmail.com>
2022-09-18 05:40:36 +0530

    Change screen orientation

---
+++ app/src/main/java/com/sudhirtheindian/changethescreenorientationgfsp/MainActivity.java
@@ -0,0 +1,19 @@
+package com.sudhirtheindian.changethescreenorientationgfsp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
